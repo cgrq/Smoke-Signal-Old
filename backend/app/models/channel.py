e1c686890b5748aa0af702a2b1fdc634caacf727
rajahwu <vmradford@gmail.com>
2023-05-05 14:06:02 -0400

    complete channel model

---
+++ backend/app/models/channel.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class Channel(db.Model):
+    __tablename__ = "channels"
+    
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    
+    # Table Keys
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(2000))
+    type = db.Column(db.String(150))
+    image_url = db.Column(db.String(255))
+    
+    # Foreign Keys
+    team_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
+    
+    # Common Keys
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+
+     
+    # Methods
+    def __repr__(self):
+        return f'<Channel id:{self.id}, name:{self.name} :: {self.created_at}>'
--- backend/app/models/message.py
+++ backend/app/models/message.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from .db import db, add_prefix_for_prod
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 
 class Message(db.Model):
