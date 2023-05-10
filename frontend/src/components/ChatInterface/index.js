8b386e612341edf8adcf65aa930bf5ff379fff31
Roosevelt Burden <rooseveltburden@gmail.com>
2023-05-10 11:58:59 -0600

    finished integrating websockets with message feed

--- frontend/src/components/ChatInterface/index.js
+++ frontend/src/components/ChatInterface/index.js
@@ -1,4 +1,4 @@
-import "./ChatInterface.css"
+import "./ChatInterface.css";
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getCurrentTeamThunk, getUserTeamsThunk } from "../../store/teams";
@@ -11,72 +11,71 @@ import DirectMessageFeed from "./DirectMessageFeed";
 import { getUserChannelsThunk } from "../../store/channels";
 
 function ChatInterface({ isLoaded }) {
-    const sessionUser = useSelector((state) => state.session.user);
-    const dispatch = useDispatch();
-    const currentTeam = useSelector((state) => state.teams.currentTeam)
-    const currentChannel = useSelector((state) => state.channels.currentChannel)
-    const userChannels = useSelector((state) => state.channels.userChannels)
-    const [newMessage, setNewMessage] = useState("");
+  const sessionUser = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+  const currentTeam = useSelector((state) => state.teams.currentTeam);
+  const currentChannel = useSelector((state) => state.channels.currentChannel);
+  const userChannels = useSelector((state) => state.channels.userChannels);
+  // const [newMessage, setNewMessage] = useState("");
 
+  useEffect(() => {
+    if (sessionUser && sessionUser.id) {
+      dispatch(getUserTeamsThunk(sessionUser.id));
+      // dispatch(getCurrentTeamThunk(currentTeam.id));
+      dispatch(getUserChannelsThunk());
+    }
+  }, [sessionUser]);
 
-    useEffect(() => {
-        if (sessionUser && sessionUser.id) {
-            dispatch(getUserTeamsThunk(sessionUser.id))
-            // dispatch(getCurrentTeamThunk(currentTeam.id));
-            dispatch(getUserChannelsThunk())
-        }
+  useEffect(() => {
+    dispatch(getCurrentTeamThunk(4));
+  }, []);
 
-    }, [sessionUser]);
-
-    useEffect(() => {
-        dispatch(getCurrentTeamThunk(4))
-    }, [])
-
-    return (
-        <div className="chat-interface-main-wrapper">
-            {/* Left Column */}
-            <div className="chat-interface-main-column chat-interface-main-left-column">
-                {/* Logo */}
-                <div></div>
-                {/* Team management*/}
-                <TeamManagement />
-                {
-                    currentTeam && userChannels
-                        ? (
-                            <>
-                                {/* Channels */}
-                                <ChannelFeed userChannels={userChannels} currentTeamId={currentTeam.id} />
-                                {/* Direct Messages */}
-                                <DirectMessageFeed userChannels={userChannels} currentTeamId={currentTeam.id} />
-                            </>
-                        )
-                        : null
-                }
-            </div>
-            {/* Right Column */}
-            <div className="chat-interface-main-column chat-interface-main-right-column">
-                {/* Nav/Search */}
-                <SearchNav isLoaded={isLoaded} sessionUser={sessionUser} />
-                {/* Message feed */}
-                {/* Message Inputs */}
-                {
-                    currentChannel
-                    ? (
-                        <>
-                            <MessageFeed />
-                            <MessageInputs channelId={currentChannel.id} newMessage={newMessage} setNewMessage={setNewMessage} />
-                        </>
-                    )
-                        :(
-                            <div className="chat-interface-no-channel-selected-wrapper">
-                                <h1>Please select a channel</h1>
-                            </div>
-                        )
-
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className="chat-interface-main-wrapper">
+      {/* Left Column */}
+      <div className="chat-interface-main-column chat-interface-main-left-column">
+        {/* Logo */}
+        <div></div>
+        {/* Team management*/}
+        <TeamManagement />
+        {currentTeam && userChannels ? (
+          <>
+            {/* Channels */}
+            <ChannelFeed
+              userChannels={userChannels}
+              currentTeamId={currentTeam.id}
+            />
+            {/* Direct Messages */}
+            <DirectMessageFeed
+              userChannels={userChannels}
+              currentTeamId={currentTeam.id}
+            />
+          </>
+        ) : null}
+      </div>
+      {/* Right Column */}
+      <div className="chat-interface-main-column chat-interface-main-right-column">
+        {/* Nav/Search */}
+        <SearchNav isLoaded={isLoaded} sessionUser={sessionUser} />
+        {currentChannel ? (
+          <>
+            {/* Message feed */}
+            <MessageFeed channelId={currentChannel.id} />
+            {/* Message Inputs */}
+            <MessageInputs
+              channelId={currentChannel.id}
+              // newMessage={newMessage}
+              // setNewMessage={setNewMessage}
+            />
+          </>
+        ) : (
+          <div className="chat-interface-no-channel-selected-wrapper">
+            <h1>Please select a channel</h1>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default ChatInterface;
--- frontend/src/components/FeedItem/index.js
+++ frontend/src/components/FeedItem/index.js
@@ -1,49 +1,50 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import OpenModalButton from "../OpenModalButton"
+import OpenModalButton from "../OpenModalButton";
 import ChannelFormModal from "../ChannelFormModal";
 import DeleteChannelModal from "../DeleteChannelModal";
-import "./FeedItem.css"
+import "./FeedItem.css";
 import { getCurrentChannelThunk } from "../../store/channels";
 import { getChannelMessagesThunk } from "../../store/messages";
 
 export default function FeedItem({ channelId, imageSrc, name }) {
-    const dispatch = useDispatch();
-    const [errors, setErrors] = useState([]);
+  const dispatch = useDispatch();
+  const [errors, setErrors] = useState([]);
 
+  const onChannelClick = async () => {
+    const data = await dispatch(getCurrentChannelThunk(channelId));
 
-    const onChannelClick = async () => {
-        const data = await dispatch(getCurrentChannelThunk(channelId));
-
-        if (data) {
-            setErrors(data);
-        } else {
-            dispatch(getChannelMessagesThunk(channelId))
-        }
+    if (data) {
+      setErrors(data);
+    } else {
+      dispatch(getChannelMessagesThunk(channelId));
     }
+  };
 
-    if (!channelId) return null
+  if (!channelId) return null;
 
-    return (
-        <>
-            <div onClick={onChannelClick} className="feed-item-wrapper clickable">
-                <img src={imageSrc} />
-                <p>{name}</p>
-            </div>
-            <div>
-                <OpenModalButton
-                    buttonText="Edit"
-                    modalComponent={
-                        <ChannelFormModal id={channelId} componentType="update" title="Update channel" />
-                    }
-                />
-                <OpenModalButton
-                    buttonText="Delete"
-                    modalComponent={
-                        <DeleteChannelModal id={channelId} />
-                    }
-                />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div onClick={onChannelClick} className="feed-item-wrapper clickable">
+        <img src={imageSrc} />
+        <p>{name}</p>
+      </div>
+      <div>
+        <OpenModalButton
+          buttonText="Edit"
+          modalComponent={
+            <ChannelFormModal
+              id={channelId}
+              componentType="update"
+              title="Update channel"
+            />
+          }
+        />
+        <OpenModalButton
+          buttonText="Delete"
+          modalComponent={<DeleteChannelModal id={channelId} />}
+        />
+      </div>
+    </>
+  );
 }
